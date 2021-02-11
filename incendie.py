5138ea4e971cd60f9aa033cc875f765eae9f9fb1
Yannis KYRIASIS <yannis.kyriasis@ens.uvsq.fr>
2021-02-11 09:10:44 +0100

    nouveay

--- incendie.py
+++ incendie.py
@@ -1,1 +1,10 @@
-bonjour
+####################
+#TD01
+#Groupe 2
+#Yannis KYRIASIS
+#Nadir HADEOUI
+#Ismael AYAD
+#Oumou ATJI
+#https://github.com/uvsq22004651/projet_incendie
+####################
+print('a')
