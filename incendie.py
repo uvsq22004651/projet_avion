189b4b513e2379d808982dc168d8df5ff9c55fa0
uvsq22004651 <71507379+uvsq22004651@users.noreply.github.com>
2021-02-17 16:55:13 +0100

    Update incendie.py

--- incendie.py
+++ incendie.py
@@ -1,5 +1,5 @@
 ################################
-#Information sur le froupe
+#Information sur le Groupe
 #MIASHS L1S2 TD1
 #Groupe 2
 #Yannis KYRIASIS
@@ -9,18 +9,27 @@
 #https://github.com/uvsq22004651/projet_incendie
 ################################
 
-################################
-# Importations de librairies
+################################# IMPORTATIONS DE LIBRAIRIES
 
 import tkinter as tk 
+import random as rd
+
+################################# CONSTANTES
+# Couleur des parcelles
+EAU="blue"
+FORET="green"
+FEU="red"
+PRAIRIE="yellow"
+CENDRES_TIEDES="grey"
+CENDRES_ETEINTES="black"
 
 
-################################
-# Constantes
+################################# LISTES
+
+color_liste = [EAU, FORET, FEU, PRAIRIE, CENDRES_TIEDES, CENDRES_ETEINTES]
 
 
-################################
-# Fonctions
+################################# FONCTIONS
 
 def quadrillage():
     """Création du quadrillage"""
@@ -29,8 +38,9 @@ def quadrillage():
             canvas.create_line((0, h), (600, h), fill='black')
             canvas.create_line((l, 0), (l, 400), fill='black')
 
-################################
-# Programme principale
+
+
+################################# PROGRAMME PRINCIPALE 
 racine = tk.Tk()
 racine.title("Incendie")
 
@@ -39,17 +49,23 @@ racine.title("Incendie")
 # Création des widgets
 
 canvas = tk.Canvas(racine, width=600, height=400, bg="white")
-
-
+bouton_commencer = tk.Button(racine, text="Commencer", command=quadrillage)
+bouton_sauvegarder = tk.Button(racine, text="Sauvegarder")
+bouton_importer = tk.Button(racine, text='Importer')
+bouton_pause = tk.Button(racine, text="Pause")
 
 
 ################################
 # Placement des widegts
 
-canvas.grid(column=0, row=0)
+canvas.grid(column=0, row=1, columnspan=4)
+bouton_commencer.grid(column=2, row=2)
+bouton_sauvegarder.grid(column=2, row=0)
+bouton_importer.grid(column=1, row=0)
+bouton_pause.grid(column=1, row=2)
 
-bouton_commencer = tk.Button(racine, text="Commencer", command=quadrillage)
-bouton_commencer.grid()
 
+################################
+# Fin de la boucle
 
 racine.mainloop()
