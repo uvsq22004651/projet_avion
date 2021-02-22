22f173ed88d616fbe01bea1bcc60a00dc3547ba4
Yannis KYRIASIS <yannis.kyriasis@ens.uvsq.fr>
2021-02-22 16:15:37 +0100

    quadrillage

--- L1S2-python/jeu_vie.py
+++ L1S2-python/jeu_vie.py
@@ -9,6 +9,7 @@
 
 ########################
 # import des librairies
+
 import tkinter as tk
 
 
@@ -16,60 +17,69 @@ import tkinter as tk
 # Constantes
 
 COULEUR_FOND = "grey100"
+COULEUR_QUADR = "grey50"
+COULEUR_VIVANT = "yellow"
 LARGEUR = 600
 HAUTEUR = 400
+# la longueur des carrés qui constituent le quadrillage
 COTE = 100
-NB_COL = LARGEUR//COTE
-NB_LINE = HAUTEUR//COTE
-COULEUR_VIVANT = 'yellow'
-COULEUR_VIVANT = 'black'
-########################
+NB_COL = LARGEUR // COTE
+NB_LINE = HAUTEUR // COTE
+
+######################
 # Variables globales
 
-
-
-
-
-
-
+tableau = None
 
 ########################
 # fonctions
+
+
 def quadrillage():
-    """Affiche un quadrillage sur le canvas"""
-    global canvas, LARGEUR, HAUTEUR
     """Affiche un quadrillage sur le canvas."""
-    for l in range(0,601,10):
-        for h in range(0,401,10):
-            canvas.create_line((0, h), (LARGEUR, h), fill = 'black')
-            canvas.create_line((l, 0), (l, HAUTEUR), fill = 'black')
+    x0, x1 = 0, LARGEUR
+    y = 0
+    while y <= HAUTEUR:
+        canvas.create_line(x0, y, x1, y, fill=COULEUR_QUADR)
+        y += COTE
+    y0, y1 = 0, LARGEUR
+    x = 0
+    while x <= LARGEUR:
+        canvas.create_line(x, y0, x, y1, fill=COULEUR_QUADR)
+        x += COTE
 
 
-def coords_to_lg(x, y):
-    """Fonctiojn qui retourne la colonne et la ligne du quadrillage à partir des coordonnées x et y"""
-    return x//COTE, y//COTE
+def coord_to_lg(x, y):
+    """Fonction qui retourne la colonne et la ligne du quadrillage
+    à partir des coordonnées x et y"""
+    return x // COTE, y // COTE
 
-def chnage_carre(event):
+
+def change_carre(event):
     """Change l'état du carré sur lequel on a cliqué"""
-    i, j = coords_to_lg(event.x, event.y)
+    i, j = coord_to_lg(event.x, event.y)
     if tableau[i][j] == -1:
-        x, y = i +COTE, j*COTE
-        carre = canvas.create_rectangle(x, y, X + COTE, y + COTE, fill = COULEUR_VIVANT, outline = COULEUR_QUADR)
+        x, y = i * COTE, j * COTE
+        carre = canvas.create_rectangle(x, y, x + COTE,
+                                        y + COTE, fill=COULEUR_VIVANT,
+                                        outline=COULEUR_QUADR)
+        tableau[i][j] = carre
+    else:
+        canvas.delete(tableau[i][j])
+        tableau[i][j] = -1
 
 
 def creer_tableau():
-    """Initialiser un tableau à deuxx dimensions qui vaut -1 partout
-    -1 est une case morte
+    """initialise un tableau à deux dimensions qui vaut -1 partout
+    -1 est pour une case morte
     identifiant du carré dessiné si une case est vivante
     tableau[i][j] est la valeur de la case à la colonne i et la ligne j
     """
     global tableau
     tableau = []
     for i in range(NB_COL):
-        tableau.append([-1]*NB_LINE)
-    tableau = [tableau_col for i in range (NB_COL)]
-    print(tableau)
-
+        tableau.append([-1] * NB_LINE)
+    # tableau = [tableau_col for i in range(NB_COL)]
 
 
 ########################
@@ -80,7 +90,10 @@ racine.title("Jeu de la vie")
 # création des widgets
 canvas = tk.Canvas(racine, bg=COULEUR_FOND, width=LARGEUR, height=HAUTEUR)
 quadrillage()
+creer_tableau()
 # placement des widgets
 canvas.grid(row=0)
+# liaison des événements
+canvas.bind("<Button-1>", change_carre)
 # boucle principale
 racine.mainloop()
