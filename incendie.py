d699ffb7ced0716aa38513c65821917271e4d1c7
uvsq22004651 <71507379+uvsq22004651@users.noreply.github.com>
2021-03-04 09:09:02 +0100

    Update incendie.py

--- incendie.py
+++ incendie.py
@@ -15,53 +15,43 @@ import tkinter as tk
 import random as rd
 
 ################################# CONSTANTES
-# Couleur des parcelles
-EAU="blue"
-FORET="green"
-PRAIRIE="yellow"
-FEU="red"
-CENDRES_TIEDES="grey"
-CENDRES_ETEINTES="black"
-
 #Création canvas et quadrillage
-LARGEUR = 600
-HAUTEUR = 400
-COTE = 25
+SIZE = 500
+COTE = 10
 
 ################################# LISTES
 
-color_liste = [EAU, FORET, PRAIRIE]
+COLOR = ["blue", "forest green", "yellow", "red", "grey", "black"]
 
 ################################# FONCTIONS
 
 def quadrillage():
     """Création du quadrillage"""
-    x0, x1 = 0, LARGEUR
+    x0, x1 = 0, SIZE
     y = 0
-    while y <= HAUTEUR:
+    while y <= SIZE:
         canvas.create_line(x0, y, x1, y, fill = "black")
         y += COTE
     
-    y0, y1 = 0, HAUTEUR
+    y0, y1 = 0, SIZE
     x = 0
-    while x <= LARGEUR:
+    while x <= SIZE:
         canvas.create_line(x, y0, x, y1, fill = "black")
         x += COTE
 
 
+
 def parcelles():
     """Création des parcelles d'eau, de forêt et de prairie"""
-    x0, y0, x1, y1 = 0, 0, COTE, COTE
-    for parcelle in range(384):
-        canvas.create_rectangle(x0, y0, x1, y1, fill = rd.choice(color_liste))
-        x0 += COTE
-        x1 += COTE
-        if x0 == LARGEUR:
-            x0 = 0
-            y0 += COTE
-            x1 = COTE
-            y1 += COTE
-            canvas.create_rectangle(x0, y0, x1, y1, fill = rd.choice(color_liste))
+    for i in range(50):
+        for j in range(50):
+            canvas.create_rectangle(i*COTE, j*COTE, (i+1)*COTE, (j+1)*COTE, fill = (rd.choice(COLOR)))
+    return i, j        
+        
+def voisins(COTE, i, j):
+    """Survole toutes les parcelles pour éxaminer leur voisins"""
+    return [(a,b) for (a, b) in [(i, j+1),(i, j-1), (i-1, j), (i+1,j)]
+    if a in range(COTE) and b in range(COTE)]
 
 
 ################################# PROGRAMME PRINCIPALE 
@@ -71,8 +61,8 @@ racine.title("Incendie")
 ################################
 # Création des widgets
 
-canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR, bg="white", borderwidth=0, highlightthickness=0)
-bouton_commencer = tk.Button(racine, text="Création des parcelles", command=lambda:[quadrillage(), parcelles()])
+canvas = tk.Canvas(racine, width=SIZE, height=SIZE, bg="white", borderwidth=0, highlightthickness=0)
+bouton_commencer = tk.Button(racine, text="Création des parcelles", command=parcelles)
 bouton_sauvegarder = tk.Button(racine, text="Sauvegarder")
 bouton_importer = tk.Button(racine, text='Importer')
 bouton_pause = tk.Button(racine, text="Pause")
